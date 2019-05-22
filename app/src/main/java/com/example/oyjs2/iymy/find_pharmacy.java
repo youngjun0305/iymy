177748f9d8e8015c8b8f6041d2c750280b601fc8
choi <wjdwodowk@naver.com>
2019-05-22 18:45:22 +0900

    find_pharmacy create

---
+++ app/src/main/java/com/example/oyjs2/iymy/find_pharmacy.java
@@ -0,0 +1,13 @@
+package com.example.oyjs2.iymy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class find_pharmacy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_pharmacy);
+    }
+}
