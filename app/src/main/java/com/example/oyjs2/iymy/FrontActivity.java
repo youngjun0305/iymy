3a979be4b7898225c08c82e15603defbed68ac94
voxltd <vox_ltd@naver.com>
2019-06-16 23:17:29 +0900

    add button image

---
+++ app/src/main/java/com/example/oyjs2/iymy/FrontActivity.java
@@ -0,0 +1,14 @@
+package com.example.oyjs2.iymy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class FrontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+    }
+}
--- app/src/main/java/com/example/oyjs2/iymy/main.java
+++ app/src/main/java/com/example/oyjs2/iymy/main.java
@@ -1,13 +1,52 @@
 package com.example.oyjs2.iymy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class main extends AppCompatActivity {
+
+    ImageButton btnGoogle, btnKakao;
+    Button btnGuest;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
+
+        btnGoogle = findViewById(R.id.google);
+        btnKakao = findViewById(R.id.kakao);
+        btnGuest = findViewById(R.id.guest);
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                intent = new Intent(main.this, FrontActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnKakao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                intent = new Intent(main.this, FrontActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                intent = new Intent(main.this, FrontActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
