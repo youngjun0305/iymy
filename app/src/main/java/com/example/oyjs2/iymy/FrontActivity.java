7061cf4dd455c33d1a577b875974783937ec379e
yj <icy9409@naver.com>
2019-06-17 17:33:30 +0900

    focus

--- app/src/main/java/com/example/oyjs2/iymy/FrontActivity.java
+++ app/src/main/java/com/example/oyjs2/iymy/FrontActivity.java
@@ -1,17 +1,23 @@
 package com.example.oyjs2.iymy;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FrontActivity extends AppCompatActivity {
 
+    ImageView leftImg, setting;
+    View imgLayout;
+    TextView titles;
     ImageButton camSearch, barSerch, pharmSearch, alarm, pill;
     Intent intent, outIntent;
     EditText search;
@@ -23,7 +29,10 @@ public class FrontActivity extends AppCompatActivity {
         setContentView(R.layout.activity_front);
 
         intent = getIntent();
-
+        leftImg = findViewById(R.id.leftImg);
+        setting = findViewById(R.id.setting);
+        imgLayout = findViewById(R.id.imgLayout);
+        titles = findViewById(R.id.titles);
         search_button = findViewById(R.id.search_button);
         search = findViewById(R.id.search);
         camSearch = findViewById(R.id.camSearch);
@@ -31,12 +40,15 @@ public class FrontActivity extends AppCompatActivity {
         pharmSearch = findViewById(R.id.pharmSearch);
         alarm = findViewById(R.id.alarm);
         pill = findViewById(R.id.pill);
+        titles.setText("00님");
+        leftImg.setImageResource(R.drawable.user);
+        setting.setImageResource(R.drawable.logout);
 
+        search.setFocusable(false);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                outIntent = new Intent(FrontActivity.this,SearchActivity.class );
+                outIntent = new Intent(FrontActivity.this, SearchActivity.class);
                 startActivity(outIntent);
             }
         });
@@ -81,7 +93,7 @@ public class FrontActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                outIntent = new Intent(FrontActivity.this, med_list_see.class );
+                outIntent = new Intent(FrontActivity.this, med_list_see.class);
                 startActivity(outIntent);
             }
         });
--- app/src/main/java/com/example/oyjs2/iymy/SearchActivity.java
+++ app/src/main/java/com/example/oyjs2/iymy/SearchActivity.java
@@ -1,13 +1,17 @@
 package com.example.oyjs2.iymy;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
 
-
+    EditText etSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,15 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         Intent intent = getIntent();
+        etSearch=findViewById(R.id.etSearch);
+
+        etSearch.requestFocus();
+        if(etSearch.isFocusableInTouchMode()==true){
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        }else{
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY,0);
+        }
     }
 }
